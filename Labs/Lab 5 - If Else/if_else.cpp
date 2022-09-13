1839c72a74ecfb09eeb696027423c5f6ced03188
Collin Franklin <collin.franklin.2003@gmail.com>
2022-09-13 11:37:21 -0500

    Update

---
+++ Labs/Lab 5 - If Else/if_else.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int x = 10, y = 4;
+    if (x <= y)
+    {
+        cout << "x = " << x << ", Y = " << y << endl;
+        cout << "x is less than or equal to y" << endl;
+    }
+    else 
+    { 
+        cout << "X = " << x << ", Y = " << y << endl; 
+        cout << "X is greater than Y" << endl; 
+    } 
+ system("pause"); 
+ return 0; 
+}
